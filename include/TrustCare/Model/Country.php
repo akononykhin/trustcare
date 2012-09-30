23492637f2238c18023de0ee936433fc35cb6795
alexkon <akononykhin@users.noreply.github.com>
2012-09-30 19:44:20 +0000

    3.1 Model for Country

---
+++ include/TrustCare/Model/Country.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ *
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ *
+ */
+
+class TrustCare_Model_Country extends TrustCare_Model_Abstract
+{
+    protected $_id;
+    protected $_iso_3166;
+    protected $_name;
+    
+    /**
+     * @param  int $value 
+     * @return TrustCare_Model_Country
+     */
+    public function setId($value)
+    {
+        $this->_parameterChanged('id', $value);
+    	$this->_id = (int) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+    
+    /**
+     * @param  string $value 
+     * @return TrustCare_Model_Country
+     */
+    public function setIso3166($value)
+    {
+        $this->_parameterChanged('iso_3166', $value);
+    	$this->_iso_3166 = (string) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getIso3166()
+    {
+        return $this->_iso_3166;
+    }
+    
+    /**
+     * @param  string $value 
+     * @return TrustCare_Model_Country
+     */
+    public function setName($value)
+    {
+        $this->_parameterChanged('name', $value);
+    	$this->_name = (string) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+    
+    
+    /**
+     * Find an entry by id
+     *
+     * @param  string $id 
+     * @param array|null $options
+     * @return TrustCare_Model_Country
+     */
+    public static function find($id, array $options = null)
+    {
+        $newEntity = new TrustCare_Model_Country($options);
+        $result = $newEntity->getMapper()->find($id, $newEntity);
+        
+        if(!$result) {
+            unset($newEntity);
+            $newEntity = null;
+        }
+        
+        return $newEntity;
+    }
+
+    /**
+     * Find an entry by iso_3166
+     *
+     * @param  string $value - login 
+     * @param array|null $options
+     * @return TrustCare_Model_Country
+     */
+    public static function findByIso($value, array $options = null)
+    {
+        $newEntity = new TrustCare_Model_Country($options);
+        $result = $newEntity->getMapper()->findByIso($value, $newEntity);
+        
+        if(!$result) {
+            unset($newEntity);
+            $newEntity = null;
+        }
+        
+        return $newEntity;
+    }
+    
+    public function delete()
+    {
+    	parent::delete();
+    	$this->id = null;
+    }
+}
---
+++ tests/module/model/country_tests.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+class TestOfCountry extends UnitTestCase {
+    private $paramsAtDb = null;
+    
+    /**
+     * 
+     * @var Zend_Db
+     */
+    private $db = null;
+    
+    function setUp() {
+        $this->db = Zend_Registry::get('dbAdapter');
+        
+        try {
+            $params = array(
+                'id' => $this->db->nextSequenceId('country_id_seq'),
+            	'iso_3166' => 't1',
+                'name' => 'Test1',
+            );
+
+            $columns = array();
+            $values = array();
+            foreach($params as $key=>$value) {
+                $columns[] = $this->db->quoteIdentifier($key);
+                if(!is_bool($value) && !is_int($value)) {
+                    $values[] = $this->db->quote($value);
+                }
+                else {
+                    $values[] = $this->db->quote($value, Zend_Db::INT_TYPE);
+                }
+            }
+            $query = sprintf("insert into country(%s) values(%s);", join(",", $columns), join(",", $values));
+            $res = $this->db->query($query);
+            $this->paramsAtDb = $params;
+        }
+        catch(Exception $ex){}
+    }
+
+
+    function tearDown() {
+        $query = sprintf("delete from country;");
+        $this->db->query($query);
+    }
+    
+
+    function testInitializing() {
+        $params = array(
+            'id' => '111',
+            'iso_3166' => 't2',
+            'name' => 'Test2',
+            'mapperOptions' => array('adapter' => $this->db)
+        );
+        
+        $model = new TrustCare_Model_Country($params);
+
+        $this->_compareObjectAndParams($model, $params);
+    }
+    
+    function testDefaultValues() {
+    }
+
+    function testLoadExistingById() {
+        $model = TrustCare_Model_Country::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        $this->_compareObjectAndParams($model, $this->paramsAtDb);
+    }
+
+    function testLoadUnexistingById() {
+        $model = TrustCare_Model_Country::find(-1 * $this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        
+        $this->assertNull($model, "This entity must not be loaded");
+    }
+
+
+    function testLoadExistingByIso() {
+        $model = TrustCare_Model_Country::findByIso($this->paramsAtDb['iso_3166'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        $this->_compareObjectAndParams($model, $this->paramsAtDb);
+    }
+
+    function testLoadUnexistingByIso() {
+        $model = TrustCare_Model_Country::findByIso($this->paramsAtDb['iso_3166'] . '_unknown', array('mapperOptions' => array('adapter' => $this->db)));
+        
+        
+        $this->assertNull($model, "This entity must not be loaded");
+    }
+    
+    function testSaveNew() {
+        $params = array(
+            'iso_3166' => 't3',
+            'name' => 'Test3',
+            );
+        
+        try {
+            $model = new TrustCare_Model_Country(array('mapperOptions' => array('adapter' => $this->db)));
+            $model->setOptions($params);
+            $model->save();
+            
+            $id = $model->id;
+            $params['id'] = $id;
+            
+            $model1 = TrustCare_Model_Country::find($params['id'], array('mapperOptions' => array('adapter' => $this->db)));
+            $this->_compareObjectAndParams($model1, $params);
+        }
+        catch(Exception $ex) {
+            $this->assertTrue(false, sprintf("Can't save new entity: %s", $ex->getMessage()));
+        }
+    }
+
+    function testChangeParameters() {
+        $model = TrustCare_Model_Country::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        if(!is_null($model)) {
+        	$params['iso_3166'] = 't3' == $model->iso_3166 ? 't4' : 't3';
+        	$params['name'] = $model->name . '22';
+        	 
+            try {
+                $model->setOptions($this->paramsAtDb);
+                $model->save();
+                
+                $model1 = TrustCare_Model_Country::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+                $this->_compareObjectAndParams($model1, $this->paramsAtDb);
+            }
+            catch(Exception $ex) {
+                $this->assertTrue(false, sprintf("Unexpected exception: %s", $ex->getMessage()));
+            }
+        }
+        else {
+            $this->assertTrue(false, "Can't initialize object");
+        }
+    }
+    
+    public function testDelete() {
+        $model = TrustCare_Model_Country::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        try {
+            $model->delete();
+            
+            $model1 = TrustCare_Model_Country::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+            $this->assertNull($model1, "Entity hasn't been deleted: %s");
+        }
+        catch(Exception $ex) {
+            $this->assertTrue(false, sprintf("Delete() thrown exception: %s", $ex->getMessage()));
+        }
+    }
+    
+    /**
+     * 
+     * @param TrustCare_Model_Country $model
+     * @param array $params
+     * @return void|void
+     */
+    private function _compareObjectAndParams($model, $params) {
+        if (is_null($model)) {
+            $this->assertTrue(false, "Entity not initialized");
+            return;
+        }
+        if(is_null($params) || !is_array($params)) {
+            $this->assertTrue(false, "Sample parameters not filled!");
+            return;
+        }
+        
+        $this->assertEqual($model->id, $params['id'], "Incorrect 'id': %s");
+        $this->assertEqual($model->iso_3166, $params['iso_3166'], "Incorrect 'iso_3166': %s");
+        $this->assertEqual($model->name, $params['name'], "Incorrect 'name': %s");
+    }
+}
+
+ 
