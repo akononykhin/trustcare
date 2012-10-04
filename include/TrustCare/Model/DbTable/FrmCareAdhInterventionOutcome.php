12ee69188ca4fb65e9a8fb7edac7185829dcd879
alexkon <akononykhin@users.noreply.github.com>
2012-10-04 20:33:00 +0000

    3.10 Model for PharmaceuticalCare form

---
+++ include/TrustCare/Model/DbTable/FrmCareAdhInterventionOutcome.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+
+class TrustCare_Model_DbTable_FrmCareAdhInterventionOutcome extends ZendX_Db_Table_Abstract
+{
+    protected $_name    = 'frm_care_adh_intervention_outcome';
+    protected $_primary = 'id';
+    
+    protected $_metadata = array (
+                'id' => 
+                    array (
+                      'SCHEMA_NAME' => NULL,
+                      'TABLE_NAME' => 'frm_care_adh_intervention_outcome',
+                      'COLUMN_NAME' => 'id',
+                      'COLUMN_POSITION' => 1,
+                      'DATA_TYPE' => 'int',
+                      'DEFAULT' => NULL,
+                      'NULLABLE' => false,
+                      'LENGTH' => NULL,
+                      'SCALE' => NULL,
+                      'PRECISION' => NULL,
+                      'UNSIGNED' => NULL,
+                      'PRIMARY' => true,
+                      'PRIMARY_POSITION' => 1,
+                      'IDENTITY' => false,
+                    ),
+              );
+    
+    
+    public function insert(array $data)
+    {
+        $db = Zend_Registry::get("Storage")->getPersistantDb(); 
+        $data['id'] = $db->nextSequenceId('frm_care_adh_intervention_outcome_id_seq');
+
+        return parent::insert($data);
+    }
+    
+}
