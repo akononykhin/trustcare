acc5d3cbe772433400f21786a11c2395c99df65e
alexkon <alexkon@0ddc3e09-b488-40ba-a23a-c64a73458e29>
2012-10-05 06:19:51 +0000

    3.10 Model for PharmaceuticalCare form

---
+++ trunk/include/TrustCare/Model/DbTable/FrmCareSuspectedAdrHepatic.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+
+class TrustCare_Model_DbTable_FrmCareSuspectedAdrHepatic extends ZendX_Db_Table_Abstract
+{
+    protected $_name    = 'frm_care_suspected_adr_hepatic';
+    protected $_primary = 'id';
+    
+    protected $_metadata = array (
+                'id' => 
+                    array (
+                      'SCHEMA_NAME' => NULL,
+                      'TABLE_NAME' => 'frm_care_suspected_adr_hepatic',
+                      'COLUMN_NAME' => 'id',
+                      'COLUMN_POSITION' => 1,
+                      'DATA_TYPE' => 'int',
+                      'DEFAULT' => NULL,
+                      'NULLABLE' => false,
+                      'LENGTH' => NULL,
+                      'SCALE' => NULL,
+                      'PRECISION' => NULL,
+                      'UNSIGNED' => NULL,
+                      'PRIMARY' => true,
+                      'PRIMARY_POSITION' => 1,
+                      'IDENTITY' => false,
+                    ),
+              );
+    
+    
+    public function insert(array $data)
+    {
+        $db = Zend_Registry::get("Storage")->getPersistantDb(); 
+        $data['id'] = $db->nextSequenceId('frm_care_suspected_adr_hepatic_id_seq');
+
+        return parent::insert($data);
+    }
+    
+}
