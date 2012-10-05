b9dc1097acbbe59ed24ba447cdf2bbaa3c9095f3
alexkon <akononykhin@users.noreply.github.com>
2012-10-05 19:16:57 +0000

    3.11 Model for CommunityPharmacy form

---
+++ include/TrustCare/Model/DbTable/FrmCommunityReferredOut.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+
+class TrustCare_Model_DbTable_FrmCommunityReferredOut extends ZendX_Db_Table_Abstract
+{
+    protected $_name    = 'frm_community_referred_out';
+    protected $_primary = 'id';
+    
+    protected $_metadata = array (
+                'id' => 
+                    array (
+                      'SCHEMA_NAME' => NULL,
+                      'TABLE_NAME' => 'frm_community_referred_out',
+                      'COLUMN_NAME' => 'id',
+                      'COLUMN_POSITION' => 1,
+                      'DATA_TYPE' => 'int',
+                      'DEFAULT' => NULL,
+                      'NULLABLE' => false,
+                      'LENGTH' => NULL,
+                      'SCALE' => NULL,
+                      'PRECISION' => NULL,
+                      'UNSIGNED' => NULL,
+                      'PRIMARY' => true,
+                      'PRIMARY_POSITION' => 1,
+                      'IDENTITY' => false,
+                    ),
+              );
+    
+    
+    public function insert(array $data)
+    {
+        $db = Zend_Registry::get("Storage")->getPersistantDb(); 
+        $data['id'] = $db->nextSequenceId('frm_community_referred_out_id_seq');
+
+        return parent::insert($data);
+    }
+    
+}
