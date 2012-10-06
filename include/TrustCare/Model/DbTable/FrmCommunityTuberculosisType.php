a8ffd36ce38309ea6678bb28ceef4c81e9af8a52
alexkon <akononykhin@users.noreply.github.com>
2012-10-06 10:07:41 +0000

    3.11 Model for CommunityPharmacy form

---
+++ include/TrustCare/Model/DbTable/FrmCommunityTuberculosisType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+
+class TrustCare_Model_DbTable_FrmCommunityTuberculosisType extends ZendX_Db_Table_Abstract
+{
+    protected $_name    = 'frm_community_tuberculosis_type';
+    protected $_primary = 'id';
+    
+    protected $_metadata = array (
+                'id' => 
+                    array (
+                      'SCHEMA_NAME' => NULL,
+                      'TABLE_NAME' => 'frm_community_tuberculosis_type',
+                      'COLUMN_NAME' => 'id',
+                      'COLUMN_POSITION' => 1,
+                      'DATA_TYPE' => 'int',
+                      'DEFAULT' => NULL,
+                      'NULLABLE' => false,
+                      'LENGTH' => NULL,
+                      'SCALE' => NULL,
+                      'PRECISION' => NULL,
+                      'UNSIGNED' => NULL,
+                      'PRIMARY' => true,
+                      'PRIMARY_POSITION' => 1,
+                      'IDENTITY' => false,
+                    ),
+              );
+    
+    
+    public function insert(array $data)
+    {
+        $db = Zend_Registry::get("Storage")->getPersistantDb(); 
+        $data['id'] = $db->nextSequenceId('frm_community_tuberculosis_type_id_seq');
+
+        return parent::insert($data);
+    }
+    
+}
---
+++ include/TrustCare/Model/FrmCommunityTuberculosisType.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ *
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ *
+ */
+
+class TrustCare_Model_FrmCommunityTuberculosisType extends TrustCare_Model_Abstract
+{
+    protected $_id;
+    protected $_id_frm_community;
+    protected $_id_pharmacy_dictionary;
+    
+    /**
+     * @param  int $value 
+     * @return TrustCare_Model_FrmCommunityTuberculosisType
+     */
+    public function setId($value)
+    {
+        $this->_parameterChanged('id', $value);
+        $this->_id = (int) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+    
+    /**
+     * @param  int $value 
+     * @return TrustCare_Model_FrmCommunityTuberculosisType
+     */
+    public function setIdFrmCommunity($value)
+    {
+        $this->_parameterChanged('id_frm_community', $value);
+        $this->_id_frm_community = (int) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getIdFrmCommunity()
+    {
+        return $this->_id_frm_community;
+    }
+    
+    /**
+     * @param  int $value 
+     * @return TrustCare_Model_FrmCommunityTuberculosisType
+     */
+    public function setIdPharmacyDictionary($value)
+    {
+        $this->_parameterChanged('id_pharmacy_dictionary', $value);
+        $this->_id_pharmacy_dictionary = (int) $value;
+        return $this;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getIdPharmacyDictionary()
+    {
+        return $this->_id_pharmacy_dictionary;
+    }
+    
+    public function isExists()
+    {
+        return !is_null($this->getId());
+    }
+    
+    
+    /**
+     * Find an entry by id
+     *
+     * @param  string $id 
+     * @param array|null $options
+     * @return TrustCare_Model_FrmCommunityTuberculosisType
+     */
+    public static function find($id, array $options = null)
+    {
+        $newEntity = new TrustCare_Model_FrmCommunityTuberculosisType($options);
+        $result = $newEntity->getMapper()->find($id, $newEntity);
+        
+        if(!$result) {
+            unset($newEntity);
+            $newEntity = null;
+        }
+        
+        return $newEntity;
+    }
+
+    
+    /**
+     * Fetch all for specified $id_frm_community
+     *
+     * @param  int $value
+     * @return TrustCare_Model_FrmCommunityTuberculosisType
+     */
+    public function fetchAllForFrmCommunity($value)
+    {
+        return $this->getMapper()->fetchAllForFrmCommunity($value);
+    }
+    
+    public function delete()
+    {
+        parent::delete();
+        $this->id = null;
+    }
+}
