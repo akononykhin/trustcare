d8df167178c42790e5af359dc2abc3f5cdcbadc9
alexkon <akononykhin@users.noreply.github.com>
2012-10-06 09:51:24 +0000

    3.11 Model for CommunityPharmacy form

--- tests/module/model/all_tests.php
+++ tests/module/model/all_tests.php
@@ -44,6 +44,7 @@ class ModelGroupTest extends GroupTest {
         $this->addTestFile(dirname(__FILE__) . '/frm_community_htc_result_tests.php');
 */        
         $this->addTestFile(dirname(__FILE__) . '/frm_community_palliative_care_type_tests.php');
+        $this->addTestFile(dirname(__FILE__) . '/frm_community_sti_type_tests.php');
         
     }
 }
