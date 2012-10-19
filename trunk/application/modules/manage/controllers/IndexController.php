fa21884448898ca51f12c2d3ac5b37e9c2c17b57
alexkon <alexkon@0ddc3e09-b488-40ba-a23a-c64a73458e29>
2012-10-19 21:13:01 +0000

    5.2.4 GUI User

--- trunk/application/modules/manage/controllers/IndexController.php
+++ trunk/application/modules/manage/controllers/IndexController.php
@@ -19,6 +19,7 @@ class IndexController extends ZendX_Controller_Action
         $form = $this->_getSettingsForm();
 
         $user = Zend_Registry::get("TrustCare_Registry_User")->getUser();
+        $pharmacyModel = TrustCare_Model_Pharmacy::find($user->getIdPharmacy());
 
         if($this->getRequest()->isPost()) {
             if ($form->isValid($this->getRequest()->getPost())) {
@@ -31,10 +32,17 @@ class IndexController extends ZendX_Controller_Action
                         throw new Exception("");
                     }
                     
-                    $user->setFullName($form->getValue("full_name"));
                     if(!empty($password)) {
                         $user->setPassword(md5($password));
                     }
+                    $user->setFirstName($form->getValue("first_name"));
+                    $user->setLastName($form->getValue("last_name"));
+                    $user->setIdCountry($form->getValue("id_country"));
+                    $user->setIdState($form->getValue("id_state"));
+                    $user->setCity($form->getValue("city"));
+                    $user->setAddress($form->getValue("address"));
+                    $user->setZip($form->getValue("zip"));
+                    $user->setPhone($form->getValue("phone"));
                     $user->save();
 
                     $form->getElement("password")->setValue('');
@@ -51,7 +59,18 @@ class IndexController extends ZendX_Controller_Action
             }
         }
         else {
-            $form->getElement("full_name")->setValue($user->getFullName());
+            $form->getElement("first_name")->setValue($user->first_name);
+            $form->getElement("last_name")->setValue($user->last_name);
+            $form->getElement("id_country")->setValue($user->id_country);
+            $form->getElement("id_state")->setValue($user->id_state);
+            $form->getElement("city")->setValue($user->city);
+            $form->getElement("address")->setValue($user->address);
+            $form->getElement("zip")->setValue($user->zip);
+            $form->getElement("phone")->setValue($user->phone);
+            
+            if(!is_null($pharmacyModel)) {
+                $form->getElement("pharmacy_name")->setValue($pharmacyModel->getName());
+            }
         }
 
         $this->view->form = $form;
@@ -64,29 +83,98 @@ class IndexController extends ZendX_Controller_Action
      */
     private function _getSettingsForm()
     {
+        $countryList = array();
+        $countryList[''] = '';
+        $model = new TrustCare_Model_Country();
+        foreach ($model->fetchAll() as $obj) {
+            $countryList[$obj->getId()] = $obj->getName();
+        }
+        
+        $stateList = array();
+        $stateList[''] = '';
+        $model = new TrustCare_Model_State();
+        foreach ($model->fetchAll() as $obj) {
+            $stateList[$obj->getId()] = $obj->getName();
+        }
+        
+        
         $form = new ZendX_Form();
-        $form->setAction($this->getRequest()->getBaseUrl() . '/' . $this->getRequest()->getControllerName() . "/settings");
         $form->setMethod('post');
 
         $tabIndex = 1;
+        $form->addElement('htmltext', 'pharmacy_name', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Pharmacy"),
+            'description'   => '',
+            'value'         => "",
+            'tabindex'      => $tabIndex++,
+            'required'      => false,
+        ));
         $form->addElement('password', 'password', array(
             'label'         => Zend_Registry::get("Zend_Translate")->_("Password"),
             'description'   => "",
             'tabindex'      => $tabIndex++,
+            'required'		=> $isCreate
         ));
-        
         $form->addElement('password', 'confirm_password', array(
             'label'         => Zend_Registry::get("Zend_Translate")->_("Confirm Password"),
             'description'   => "",
             'tabindex'      => $tabIndex++,
         ));
-        
-        $form->addElement('text', 'full_name', array(
-            'label'         => Zend_Registry::get("Zend_Translate")->_("Full Name"),
+        $form->addElement('text', 'first_name', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("First Name"),
             'description'   => "",
+            'size'          => 32,
             'tabindex'      => $tabIndex++,
         ));
-        
+        $form->addElement('text', 'last_name', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Last Name"),
+            'description'   => "",
+            'size'          => 32,
+            'tabindex'      => $tabIndex++,
+        ));
+        $form->addElement('select', 'id_country', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Country"),
+            'tabindex'      => $tabIndex++,
+            'required'      => false,
+            'multioptions'  => $countryList,
+            'description'   => '',
+        ));
+        $form->addElement('select', 'id_state', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("State"),
+            'tabindex'      => $tabIndex++,
+            'required'      => false,
+            'multioptions'  => $stateList,
+            'description'   => '',
+        ));
+        $form->addElement('text', 'city', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("City"),
+            'description'   => "",
+            'size'          => 16,
+            'tabindex'      => $tabIndex++,
+            'required'      => false
+        ));
+        $form->addElement('text', 'address', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Address"),
+            'description'   => "",
+            'size'          => 32,
+            'tabindex'      => $tabIndex++,
+            'required'      => false
+        ));
+        $form->addElement('text', 'zip', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Zip"),
+            'description'   => "",
+            'size'          => 16,
+            'tabindex'      => $tabIndex++,
+            'required'      => false
+        ));
+        $form->addElement('text', 'phone', array(
+            'label'         => Zend_Registry::get("Zend_Translate")->_("Phone"),
+            'description'   => "",
+            'size'          => 16,
+            'tabindex'      => $tabIndex++,
+            'required'      => false
+        ));
+                
         $form->addElement('submit', 'send', array(
             'label'     => Zend_Registry::get("Zend_Translate")->_("Save"),
             'tabindex'  => $tabIndex++,
