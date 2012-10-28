4866933bfc07d126e263c4bef837ec0863ea1535
alexkon <akononykhin@users.noreply.github.com>
2012-10-28 18:39:50 +0000

    5.3.1 GUI Pharmaceutical care

--- application/modules/manage/controllers/FormController.php
+++ application/modules/manage/controllers/FormController.php
@@ -168,26 +168,188 @@ class FormController extends ZendX_Controller_Action
     private function _createCareForm()
     {
         if($this->getRequest()->isPost()) {
-            if ($form->isValid($this->getRequest()->getPost())) {
+            $errorMsg = Zend_Registry::get("Zend_Translate")->_("Internal Error");
+            
+            $db_options = Zend_Registry::get('dbOptions');
+            $db = Zend_Db::factory($db_options['adapter'], $db_options['params']);
+            $db->beginTransaction();
+            try {
+                $idPatient = $this->_getParam('id_patient');
+                if(empty($idPatient)) {
+                    $errorMsg = Zend_Registry::get("Zend_Translate")->_("Necessary to choose patient");
+                    throw new Exception('');
+                }
+                $dateOfVisit = $this->_getParam('date_of_visit');
+                if(empty($dateOfVisit)) {
+                    $errorMsg = Zend_Registry::get("Zend_Translate")->_("Necessary to enter date of visit");
+                    throw new Exception('');
+                }
                 
-                $errorMsg = Zend_Registry::get("Zend_Translate")->_("Internal Error");
-                try {
+                $patientModel = TrustCare_Model_Patient::find($idPatient);
+                if(is_null($patientModel)) {
+                    throw new Exception(sprintf("Trying to create form for unknown patient.id=%s", $idPatient));
+                }
+                $isPregnant = $this->_getParam('is_pregnant');
+                $isReceivePrescription = $this->_getParam('is_receive_prescription');
+                $isMedErrorScreened = $this->_getParam('is_med_error_screened');
+                $isMedErrorIdentified = $this->_getParam('is_med_error_identified');
+                $isMedAdhProblemScreened = $this->_getParam('is_med_adh_problem_screened');
+                $isMedAdhProblemIdentified = $this->_getParam('is_med_adh_problem_identified');
+                $isAdhInterventionProvided = $this->_getParam('is_adh_intervention_provided');
+                $isAdrScreened = $this->_getParam('is_adr_screened');
+                $isAdrSymptoms = $this->_getParam('is_adr_symptoms');
+                $adrSeverityId = $this->_getParam('adr_severity_id');
+                $adrStartDate = $this->_getParam('adr_start_date');
+                $adrStopDate = $this->_getParam('adr_stop_date');
+                
+                $frmModel = new TrustCare_Model_FrmCare(
+                    array(
+                		'id_patient' => $idPatient,
+                		'date_of_visit' => $dateOfVisit.' 00:00:00',
+                		'is_pregnant' => $isPregnant,
+                		'is_receive_prescription' => $isReceivePrescription,
+                		'is_med_error_screened' => $isMedErrorScreened,
+                		'is_med_error_identified' => $isMedErrorIdentified,
+                		'is_med_adh_problem_screened' => $isMedAdhProblemScreened,
+                		'is_med_adh_problem_identified' => $isMedAdhProblemIdentified,
+                		'is_adh_intervention_provided' => $isAdhInterventionProvided,
+                		'is_adr_screened' => $isAdrScreened,
+                		'is_adr_symptoms' => $isAdrSymptoms,
+                		'adr_severity_id' => $adrSeverityId,
+                'is_adr_intervention_provided' => true,
+                'is_nafdac_adr_filled' => false,
+                'is_patient_younger_15' => true,
+                'is_patient_male' => false,
 
-                    $this->getRedirector()->gotoSimpleAndExit('list', $this->getRequest()->getControllerName());
+                    	'mapperOptions' => array('adapter' => $db)
+                    )
+                );
+                if(!empty($adrStartDate)) {
+                    $frmModel->setAdrStartDate($adrStartDate . ' 00:00:00');
                 }
-                catch(Exception $ex) {
-                    $message = $ex->getMessage();
-                    if(!empty($message)) {
-                        $this->getLogger()->error($message);
-                    }
+                if(!empty($adrStopDate)) {
+                    $frmModel->setAdrStopDate($adrStopDate . ' 00:00:00');
                 }
-                $form->addError($errorMsg);
+                $frmModel->save();
+                
+                $medErrorTypes = $this->_getParam('med_error_type');
+                if(!$isMedErrorIdentified) {
+                    $medErrorTypes = array();
+                }
+                foreach($medErrorTypes  as $dictId) {
+                    $model = new TrustCare_Model_FrmCareMedErrorType(
+                        array(
+          					'id_frm_care' => $frmModel->getId(),
+           					'id_pharmacy_dictionary' => $dictId,
+                           	'mapperOptions' => array('adapter' => $db)
+                        )
+                    );
+                    $model->save();
+                }
+                
+                $medAdhProblems = $this->_getParam('med_adh_problem');
+                if(!$isMedAdhProblemIdentified) {
+                    $medAdhProblems = array();
+                }
+                foreach($medAdhProblems  as $dictId) {
+                    $model = new TrustCare_Model_FrmCareMedAdhProblem(
+                        array(
+          					'id_frm_care' => $frmModel->getId(),
+           					'id_pharmacy_dictionary' => $dictId,
+                           	'mapperOptions' => array('adapter' => $db)
+                        )
+                    );
+                    $model->save();
+                }
+                
+                $adhInterventions = $this->_getParam('adh_intervention');
+                if(!$isAdhInterventionProvided) {
+                    $adhInterventions = array();
+                }
+                foreach($adhInterventions  as $dictId) {
+                    $model = new TrustCare_Model_FrmCareAdhIntervention(
+                        array(
+          					'id_frm_care' => $frmModel->getId(),
+           					'id_pharmacy_dictionary' => $dictId,
+                           	'mapperOptions' => array('adapter' => $db)
+                        )
+                    );
+                    $model->save();
+                }
+                
+                $adhInterventionOutcomes = $this->_getParam('adh_intervention_outcome');
+                foreach($adhInterventionOutcomes  as $dictId) {
+                    $model = new TrustCare_Model_FrmCareAdhInterventionOutcome(
+                        array(
+          					'id_frm_care' => $frmModel->getId(),
+           					'id_pharmacy_dictionary' => $dictId,
+                           	'mapperOptions' => array('adapter' => $db)
+                        )
+                    );
+                    $model->save();
+                }
+                
+                throw new Exception('');
+
+                $db->commit();
+                $this->getRedirector()->gotoSimpleAndExit('list', $this->getRequest()->getControllerName());
             }
+            catch(Exception $ex) {
+                $db->rollback();
+                $message = $ex->getMessage();
+                if(!empty($message)) {
+                    $this->getLogger()->error($message);
+                }
+            }
+            $this->view->error = $errorMsg;
         }
+        else {
+            $idPatient = null;
+            $dateOfVisit = $this->convertDateToUserTimezone(gmdate("Y-m-d"), 'yyyy-MM-dd');
+            $isPregnant = false;
+            $isReceivePrescription = true;
+            $isMedErrorScreened = true;
+            $isMedErrorIdentified = true;
+            $medErrorTypes = array();
+            $isMedAdhProblemScreened = true;
+            $isMedAdhProblemIdentified = true;
+            $medAdhProblems = array();
+            $isAdhInterventionProvided = true;
+            $adhInterventions = array();
+            $adhInterventionOutcomes = array();
+            $isAdrScreened = true;
+            $isAdrSymptoms = false;
+            $adrSeverityId = null;
+            $adrStartDate = '';
+            $adrStopDate = '';
+        }
+        
+        $dictEntities = array(
+            TrustCare_Model_PharmacyDictionary::DTYPE_MEDICATION_ERROR_TYPE => $medErrorTypes,
+            TrustCare_Model_PharmacyDictionary::DTYPE_MEDICATION_ADH_PROBLEM => $medAdhProblems,
+            TrustCare_Model_PharmacyDictionary::DTYPE_ADH_INTERVENTION_PROVIDED => $adhInterventions,
+            TrustCare_Model_PharmacyDictionary::DTYPE_ADH_INTERVENTION_OUTCOME => $adhInterventionOutcomes,
+            TrustCare_Model_PharmacyDictionary::DTYPE_ADR_SEVERITY_GRADE => array($adrSeverityId),
+            );
         
         $this->view->type = 'care';
         $this->view->allow_create_patient = Zend_Registry::get("Zend_Acl")->isAllowed(Zend_Registry::get("TrustCare_Registry_User")->getUser()->role, "resource:admin.patient", "create");
-        $this->view->currentDate = $this->convertDateToUserTimezone(gmdate("Y-m-d"));
+        
+        $this->view->id_patient = $idPatient;
+        $this->view->dateOfVisit = $dateOfVisit;
+        $this->view->isPregnant = $isPregnant;
+        $this->view->isReceivePrescription = $isReceivePrescription;
+        $this->view->isMedErrorScreened = $isMedErrorScreened;
+        $this->view->isMedErrorIdentified = $isMedErrorIdentified;
+        $this->view->isMedAdhProblemScreened = $isMedAdhProblemScreened;
+        $this->view->isMedAdhProblemIdentified = $isMedAdhProblemIdentified;
+        $this->view->isAdhInterventionProvided = $isAdhInterventionProvided;
+        $this->view->isAdrScreened = $isAdrScreened;
+        $this->view->isAdrSymptoms = $isAdrSymptoms;
+        $this->view->adrStartDate = $adrStartDate;
+        $this->view->adrStopDate = $adrStopDate;
+        $this->view->dictEntities = $dictEntities;
+        
         $this->render('create-care');
         return;
     }
