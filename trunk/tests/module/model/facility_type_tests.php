70a1b795158a7b16dc7b13e2939bb1321199103a
alexkon <alexkon@0ddc3e09-b488-40ba-a23a-c64a73458e29>
2013-01-16 20:23:23 +0000

    Model of FacilityType

---
+++ trunk/tests/module/model/facility_type_tests.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * 
+ * Alexey Kononykhin
+ * alexey.kononykhin@gmail.com
+ * 
+ */
+
+class TestOfFacilityType extends UnitTestCase {
+    private $paramsAtDb = null;
+    
+    /**
+     * 
+     * @var Zend_Db
+     */
+    private $db = null;
+    
+    function setUp() {
+        $this->db = Zend_Registry::get('dbAdapter');
+        
+        try {
+            $params = array(
+                'id' => $this->db->nextSequenceId('facility_type_id_seq'),
+                'name' => 'Test1',
+            );
+
+            $columns = array();
+            $values = array();
+            foreach($params as $key=>$value) {
+                $columns[] = $this->db->quoteIdentifier($key);
+                if(!is_bool($value) && !is_int($value)) {
+                    $values[] = $this->db->quote($value);
+                }
+                else {
+                    $values[] = $this->db->quote($value, Zend_Db::INT_TYPE);
+                }
+            }
+            $query = sprintf("insert into facility_type(%s) values(%s);", join(",", $columns), join(",", $values));
+            $res = $this->db->query($query);
+            $this->paramsAtDb = $params;
+        }
+        catch(Exception $ex){}
+    }
+
+
+    function tearDown() {
+        $query = sprintf("delete from facility_type;");
+        $this->db->query($query);
+        
+        $query = sprintf("update db_sequence set value=1 where name='facility_type_id_seq';");
+        $this->db->query($query);
+    }
+    
+
+    function testInitializing() {
+        $params = array(
+            'id' => '111',
+            'name' => 'Test2',
+            'mapperOptions' => array('adapter' => $this->db)
+        );
+        
+        $model = new TrustCare_Model_FacilityType($params);
+
+        $this->_compareObjectAndParams($model, $params);
+    }
+    
+    function testDefaultValues() {
+    }
+
+    function testLoadExistingById() {
+        $model = TrustCare_Model_FacilityType::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        $this->_compareObjectAndParams($model, $this->paramsAtDb);
+    }
+
+    function testLoadUnexistingById() {
+        $model = TrustCare_Model_FacilityType::find(-1 * $this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        
+        $this->assertNull($model, "This entity must not be loaded");
+    }
+
+
+    function testSaveNew() {
+        $params = array(
+            'name' => 'Test3',
+            );
+        
+        try {
+            $model = new TrustCare_Model_FacilityType(array('mapperOptions' => array('adapter' => $this->db)));
+            $model->setOptions($params);
+            $model->save();
+            
+            $id = $model->id;
+            $params['id'] = $id;
+            
+            $model1 = TrustCare_Model_FacilityType::find($params['id'], array('mapperOptions' => array('adapter' => $this->db)));
+            $this->_compareObjectAndParams($model1, $params);
+        }
+        catch(Exception $ex) {
+            $this->assertTrue(false, sprintf("Can't save new entity: %s", $ex->getMessage()));
+        }
+    }
+
+    function testChangeParameters() {
+        $model = TrustCare_Model_FacilityType::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        if(!is_null($model)) {
+            $params['name'] = $model->name . '22';
+             
+            try {
+                $model->setOptions($params);
+                $model->save();
+                
+                $params['id'] = $model->id;
+                
+                $model1 = TrustCare_Model_FacilityType::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+                $this->_compareObjectAndParams($model1, $params);
+            }
+            catch(Exception $ex) {
+                $this->assertTrue(false, sprintf("Unexpected exception: %s", $ex->getMessage()));
+            }
+        }
+        else {
+            $this->assertTrue(false, "Can't initialize object");
+        }
+    }
+    
+    public function testDelete() {
+        $model = TrustCare_Model_FacilityType::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+        
+        try {
+            $model->delete();
+            
+            $model1 = TrustCare_Model_FacilityType::find($this->paramsAtDb['id'], array('mapperOptions' => array('adapter' => $this->db)));
+            $this->assertNull($model1, "Entity hasn't been deleted: %s");
+        }
+        catch(Exception $ex) {
+            $this->assertTrue(false, sprintf("Delete() thrown exception: %s", $ex->getMessage()));
+        }
+    }
+    
+    /**
+     * 
+     * @param TrustCare_Model_FacilityType $model
+     * @param array $params
+     * @return void|void
+     */
+    private function _compareObjectAndParams($model, $params) {
+        if (is_null($model)) {
+            $this->assertTrue(false, "Entity not initialized");
+            return;
+        }
+        if(is_null($params) || !is_array($params)) {
+            $this->assertTrue(false, "Sample parameters not filled!");
+            return;
+        }
+        
+        $this->assertEqual($model->id, $params['id'], "Incorrect 'id': %s");
+        $this->assertEqual($model->name, $params['name'], "Incorrect 'name': %s");
+    }
+}
+
+ 
