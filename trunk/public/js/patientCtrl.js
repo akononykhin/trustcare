4b0683f4032234e4f26d539c7dc341d94f115cee
alexkon <alexkon@0ddc3e09-b488-40ba-a23a-c64a73458e29>
2013-06-16 20:29:51 +0000

    Moving JS to special files

---
+++ trunk/public/js/patientCtrl.js
@@ -0,0 +1,52 @@
+var patientCtrl = {
+	 idDlgAddPatient: 'dialog-add-patient'
+	,idHidden: undefined
+	,idText: undefined
+	,newDlg: undefined
+	
+    ,init: function(idHidden, idText, newDlgTitle) {
+    	this.idHidden = '#' + idHidden;
+        this.idText = '#' + idText;
+    	
+        $(this.idText).autocomplete({
+            source:     internalAddress.patientArray()
+            ,delay:     300
+            ,focus: function(event, ui ) {
+                $(patientCtrl.idText).val(ui.item.label);
+                return false;
+            }
+            ,select: function( event, ui ) {
+                $(patientCtrl.idText).val(ui.item.label);
+                $(patientCtrl.idHidden).val(ui.item.value);
+                if(patientCtrl.checkOnChangePatient && $.isFunction(patientCtrl.checkOnChangePatient)) {
+                    patientCtrl.checkOnChangePatient();
+                }
+                return false;
+            }
+        });
+    	
+    	
+        this.newDlg = $("<div id='" + this.idDlgAddPatient + "'></div>").dialog({
+            dialogClass: 'dialog-content'
+           ,title: newDlgTitle
+           ,autoOpen: false
+           ,height: 500
+           ,width: 500
+           ,modal: true
+        });
+        
+        return this.newDlg;
+    }
+
+    ,configureAddLink: function(idAddLink) {
+        $("#" + idAddLink).click(function() {
+            patientCtrl.newDlg.load(internalAddress.patientCreateDlg(), function(){
+                pageAsDlg.linkFormToDlg("#" + patientCtrl.idDlgAddPatient);
+            }).dialog('open');
+            return false;
+        });
+    }
+    
+    ,checkOnChangePatient: undefined
+
+}
